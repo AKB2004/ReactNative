cda4b720dcb6fad44c7ae0501b079cc3686e04c9
Abhinash@2004 <bejabhinash@gmail.com>
2025-02-20 04:47:48 +0530

    View,Button,Text

---
+++ App.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable jsx-quotes */
+import {Button, Text,View} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+   <Text style={{ fontSize:30 }}>react-native mobile app</Text>
+   <Text style={{ fontSize:30 }}>react-native </Text>
+   <Button title='press here' />
+   </View>
+  );
+};
+
+export default App;
